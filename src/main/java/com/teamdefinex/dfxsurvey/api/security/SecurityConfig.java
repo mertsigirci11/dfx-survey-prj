cc8a2127c024605696a732bfa22967c8efa74d2a
BurakKontas <aburakkontas@hotmail.com>
2025-12-03 17:20:15 +0300

    exception handling

--- src/main/java/com/teamdefinex/dfxsurvey/api/security/SecurityConfig.java
+++ src/main/java/com/teamdefinex/dfxsurvey/api/security/SecurityConfig.java
@@ -1,16 +1,23 @@
 package com.teamdefinex.dfxsurvey.api.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.teamdefinex.dfxsurvey.dto.result.Result;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
@@ -25,11 +32,34 @@ public class SecurityConfig {
                     ).permitAll()
                     .anyRequest().authenticated()
             )
+                .exceptionHandling(exceptions -> exceptions
+                        .authenticationEntryPoint(customAuthenticationEntryPoint())
+                        .accessDeniedHandler(customAccessDeniedHandler()) )
             .oauth2ResourceServer(oauth2 -> oauth2
                     .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
             );
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationEntryPoint customAuthenticationEntryPoint() {
+        return (request, response, authException) -> {
+            response.setContentType("application/json");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            Result<Object> result = Result.failure("401", "Unauthorized", authException.getMessage());
+            response.getWriter().write(objectMapper.writeValueAsString(result));
+        };
+    }
+
+    @Bean
+    public AccessDeniedHandler customAccessDeniedHandler() {
+        return (request, response, accessDeniedException) -> {
+            response.setContentType("application/json");
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            Result<Object> result = Result.failure("403", "Forbidden", accessDeniedException.getMessage());
+            response.getWriter().write(objectMapper.writeValueAsString(result));
+        };
     }
 
     private JwtAuthenticationConverter jwtAuthenticationConverter() {
