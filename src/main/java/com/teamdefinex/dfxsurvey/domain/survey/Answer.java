caab65b63fddfced5c2cfd2aba5412300c9a46b6
Mert Sigirci <mertsgrci@hotmail.com>
2025-12-03 15:31:52 +0300

    answer repository&domain

---
+++ src/main/java/com/teamdefinex/dfxsurvey/domain/survey/Answer.java
@@ -0,0 +1,24 @@
+package com.teamdefinex.dfxsurvey.domain.survey;
+
+
+import com.teamdefinex.dfxsurvey.domain.base.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "question")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(callSuper = false, onlyExplicitlyIncluded = true)
+public class Answer extends BaseEntity {
+    private String participantToken;
+    private UUID surveyId;
+    private UUID questionId;
+    private String answer;
+}
